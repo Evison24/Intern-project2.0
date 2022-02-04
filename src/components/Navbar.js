afb84c44250b96b9b309c0b470d90b00bd7c51e6
Evi <evison.ndoni1@gmail.com>
2022-02-04 17:39:39 +0100

    Added react-icons

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,36 +5,28 @@ import {
   Heading,
   Button,
   Container,
+  Icon,
 } from '@chakra-ui/react';
+import { FcShop } from 'react-icons/all';
+import { NavLink } from 'react-router-dom';
+import Login from './Login';
 
 const Navbar = () => {
   return (
     <>
       <Box w="100%" maxH="xs" bgColor="black">
         <Container maxW="container.xl">
-          <Flex>
+          <Flex align="center">
             <Box p="2">
               <Heading size="md" color="orange">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="25"
-                  height="25"
-                  fill="currentColor"
-                  className="bi bi-bullseye"
-                  viewBox="0 0 16 16"
-                >
-                  <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
-                  <path d="M8 13A5 5 0 1 1 8 3a5 5 0 0 1 0 10zm0 1A6 6 0 1 0 8 2a6 6 0 0 0 0 12z" />
-                  <path d="M8 11a3 3 0 1 1 0-6 3 3 0 0 1 0 6zm0 1a4 4 0 1 0 0-8 4 4 0 0 0 0 8z" />
-                  <path d="M9.5 8a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z" />
-                </svg>
+                <Icon as={FcShop} w={8} h={8} />
               </Heading>
             </Box>
-            <Box p="2">
+            <NavLink p="2" to="/">
               <Heading size="md" color="orange">
                 WeShop
               </Heading>
-            </Box>
+            </NavLink>
             <Spacer />
             <Box>
               <Button colorScheme="orange" mr="4">
@@ -50,3 +42,9 @@ const Navbar = () => {
 };
 
 export default Navbar;
+
+{
+  /* <NavLink p="2" bgColor="orange" to="/">
+                Log in
+              </NavLink> */
+}
