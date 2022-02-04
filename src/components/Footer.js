c8b59c56600b0dd54c1adebd5e6a2bf72d6c0efd
Evi <evison.ndoni1@gmail.com>
2022-02-04 17:38:54 +0100

    Created Footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import { Box, Center } from '@chakra-ui/react';
+
+const Footer = () => {
+  return (
+    <>
+      <Box w="100%" bgColor="black" mt="30px">
+        <Center color="whiteAlpha.700" fontSize="25px">
+          © 2022 Copyright: WeShop
+        </Center>
+      </Box>
+    </>
+  );
+};
+
+export default Footer;
