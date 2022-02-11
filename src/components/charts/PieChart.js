638ac87c9cd54c29ccf0ec864426a8e9b5c261f3
Evi <evison.ndoni1@gmail.com>
2022-02-11 17:51:47 +0100

    Adedd pie chart

---
+++ src/components/charts/PieChart.js
@@ -0,0 +1,37 @@
+import * as am5 from '@amcharts/amcharts5';
+import * as am5percent from '@amcharts/amcharts5/percent';
+import am5themes_Animated from '@amcharts/amcharts5/themes/Animated';
+
+import { useEffect } from 'react';
+const PieChart = ({ data, chartSeries }) => {
+  useEffect(() => {
+    const root = am5.Root.new('chartdiv');
+
+    root.setThemes([am5themes_Animated.new(root)]);
+
+    let chart = root.container.children.push(
+      am5percent.PieChart.new(root, {
+        layout: root.verticalLayout,
+      })
+    );
+
+    // Create series
+    let series = chart.series.push(am5percent.PieSeries.new(root, chartSeries));
+    series.data.setAll(data);
+
+    // Add legend
+    let legend = chart.children.push(
+      am5.Legend.new(root, {
+        centerX: am5.percent(50),
+        x: am5.percent(50),
+        layout: root.horizontalLayout,
+      })
+    );
+
+    legend.data.setAll(series.dataItems);
+  });
+
+  return <div id="chartdiv" style={{ width: '100%', height: '500px' }}></div>;
+};
+
+export default PieChart;
