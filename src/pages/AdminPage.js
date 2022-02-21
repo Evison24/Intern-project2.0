e6854d28bdfc15c87eef93db2f7d46d18a4ec018
Evi <evison.ndoni1@gmail.com>
2022-02-21 17:58:21 +0100

    Added delete user functionality

--- src/pages/AdminPage.js
+++ src/pages/AdminPage.js
@@ -16,14 +16,21 @@ import {
   ModalCloseButton,
   useDisclosure,
   Button,
+  useToast,
+  Container,
 } from '@chakra-ui/react';
 import { useEffect, useState } from 'react';
 import Axios from '../utils/axios/Axios';
+import AdminAddUser from '../components/admin/AdminAddUser';
 import { MdDeleteForever } from 'react-icons/all';
 
 const AdminPage = () => {
   const [users, setUsers] = useState(null);
+  const [userToDelete, setUserToDelete] = useState(null);
   const { isOpen, onOpen, onClose } = useDisclosure();
+
+  const toast = useToast();
+
   useEffect(() => {
     const fetchUsers = async () => {
       const resUsers = await Axios.get('Users');
@@ -35,62 +42,80 @@ const AdminPage = () => {
   return (
     <>
       <Heading textAlign="center">Welcome Admin !</Heading>
-      <Table
-        mx={'auto'}
-        maxW={1500}
-        mt={10}
-        variant="simple"
-        bgColor={'blackAlpha.50'}
-        colorScheme={'blackAlpha'}
-      >
-        <Thead>
-          <Tr>
-            <Th>#</Th>
-            <Th>Username</Th>
-            <Th>First Name</Th>
-            <Th>Last Name</Th>
-            <Th>Email</Th>
-          </Tr>
-        </Thead>
-        <Tbody>
-          {users?.map((user, index) => (
-            <Tr key={index}>
-              <Td>{index}</Td>
-              <Td>{user.username}</Td>
-              <Td>{user.name}</Td>
-              <Td>{user.lastname}</Td>
-              <Td>{user.email}</Td>
+      <AdminAddUser setUsers={setUsers} />
 
-              <IconButton
-                aria-label="Search database"
-                colorScheme={'red'}
-                icon={<MdDeleteForever />}
-                fontSize={30}
-                onClick={onOpen}
-              />
+      <Container maxW={1500} mb={20}>
+        <Table
+          mx={'auto'}
+          mt={10}
+          variant="simple"
+          bgColor={'blackAlpha.50'}
+          colorScheme={'blackAlpha'}
+        >
+          <Thead>
+            <Tr>
+              <Th>#</Th>
+              <Th>Username</Th>
+              <Th>First Name</Th>
+              <Th>Last Name</Th>
+              <Th>Email</Th>
             </Tr>
-          ))}
-          {/* <Tr>
-            <Td>Test</Td>
-            <Td>Test</Td>
-            <Td>Test</Td>
-            <Td>Test</Td>
-            <Td>Test</Td>
-          </Tr> */}
-        </Tbody>
-      </Table>
+          </Thead>
+          <Tbody>
+            {users?.map((user, index) => (
+              <Tr key={index}>
+                <Td>{index}</Td>
+                <Td>{user.username}</Td>
+                <Td>{user.name}</Td>
+                <Td>{user.surname}</Td>
+                <Td>{user.email}</Td>
+                <Td>
+                  <IconButton
+                    aria-label="Search database"
+                    colorScheme={'red'}
+                    icon={<MdDeleteForever />}
+                    fontSize={30}
+                    onClick={() => {
+                      setUserToDelete(user.id);
+                      onOpen();
+                    }}
+                  />
+                </Td>
+              </Tr>
+            ))}
+          </Tbody>
+        </Table>
+      </Container>
 
       <Modal onClose={onClose} isOpen={isOpen} isCentered>
         <ModalOverlay />
-        <ModalContent bgColor={'blackAlpha.200'}>
+        <ModalContent bgColor={'blackAlpha'}>
           <ModalHeader bgColor={'red.400'}>Delete user?</ModalHeader>
           <ModalCloseButton />
-          <ModalBody bgColor={'blackAlpha.50'}>
-            Any data of the user will be lost !
-          </ModalBody>
+          <ModalBody>Any data of the user will be lost !</ModalBody>
           <ModalFooter>
             <Button onClick={onClose}>Cancel</Button>
-            <Button ml={5} variant="solid" bgColor={'red.400'}>
+            <Button
+              ml={5}
+              variant="solid"
+              bgColor={'red.400'}
+              onClick={() => {
+                Axios.delete(`Users/${userToDelete}`);
+                const copyUsers = [...users];
+                const index = users.findIndex(u => u.id === userToDelete);
+                copyUsers.splice(index, 1);
+                setUsers(copyUsers);
+                setUserToDelete(null);
+                onClose();
+                toast({
+                  title: 'Success.',
+                  description: 'User deleted!',
+                  status: 'success',
+                  duration: 5000,
+                  isClosable: true,
+                });
+              }}
+            >
               Delete
             </Button>
           </ModalFooter>
