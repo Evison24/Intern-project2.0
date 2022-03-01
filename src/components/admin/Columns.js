b1ee3be6a2d73e2af46ee3186de4473b0d0a1f94
Evi <evison.ndoni1@gmail.com>
2022-03-01 17:46:27 +0100

    New

---
+++ src/components/admin/Columns.js
@@ -0,0 +1,22 @@
+export const COLUMNS = [
+  {
+    Header: 'First name',
+    accessor: 'emer',
+  },
+  {
+    Header: 'Last name',
+    accessor: 'mbiemer',
+  },
+  {
+    Header: 'Username',
+    accessor: 'username',
+  },
+  {
+    Header: 'Email',
+    accessor: 'email',
+  },
+  {
+    Header: 'Nr tel',
+    accessor: 'nr_Tel',
+  },
+];
